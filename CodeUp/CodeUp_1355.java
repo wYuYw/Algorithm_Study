0f7517390af620217519aff483e53a22b37c43b3
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-18 17:48:21 +0900

    Add solution by Pro.Hong

--- CodeUp/CodeUp_1355.java
+++ CodeUp/CodeUp_1355.java
@@ -40,3 +40,22 @@ public class Main {
     }
 }
 */
+
+
+//Solution by Pro.Hong
+/*
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int j=n; j>=1; j--) {
+            for(int i=0; i<(n-j); i++) System.out.print(" ");
+            for(int i=0; i<j; i++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
+*/
