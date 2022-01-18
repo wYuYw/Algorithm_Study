c36cb4f4ee26ec54b7a9839e0b8945b4654a37b3
smj227b <80404571+smj227b@users.noreply.github.com>
2022-01-18 14:20:34 +0900

    Update CodeUp_1278.java
    220118  ||  CodeUp 1278  손민지 작성 코드 추가

--- CodeUp/CodeUp_1278.java
+++ CodeUp/CodeUp_1278.java
@@ -16,3 +16,23 @@ public class Main {
         System.out.println(cnt);
     }
 }
+
+
+
+/* 220118_손민지
+import java.util.Scanner;
+
+public class CU1278 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		int count = 1;
+		while(num/10 != 0) {
+			count++;
+			num = num/10;
+		}
+		System.out.println(count);
+	}
+}
+*/
