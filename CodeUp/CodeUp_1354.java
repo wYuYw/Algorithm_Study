01c9e5b2eb10a568d13a35567d31d62a00abdf07
smj227b <80404571+smj227b@users.noreply.github.com>
2022-01-18 17:26:28 +0900

    Update CodeUp_1354.java

--- CodeUp/CodeUp_1354.java
+++ CodeUp/CodeUp_1354.java
@@ -13,3 +13,23 @@ public class Main {
         }
     }
 }
+
+
+
+/* 220118_손민지
+import java.util.Scanner;
+
+public class CU1354 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=n; i>=1; i--) {
+			for(int j=i; j>=1; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+*/
