ad20e57ab59d3c95964905bc715229af9dd27e22
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-18 17:26:12 +0900

    Update solution by wYuYw

--- CodeUp/CodeUp_1355.java
+++ CodeUp/CodeUp_1355.java
@@ -16,3 +16,24 @@ public class CU1355 {
 		}
 	}
 }
+
+
+//Solution by wYuYw
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n;i++) {
+            if(i!=0) {
+                for(int k=0; k<i; k++) {
+                    System.out.print(" ");
+                }
+            }
+            for(int j=0; j<(n-i); j++) {
+                System.out.print("*");
+            } System.out.println();
+        }
+    }
+}
