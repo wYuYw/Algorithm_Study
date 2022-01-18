ebcf9d779a3db91fd5747a5b67cf03bf46601621
smj227b <80404571+smj227b@users.noreply.github.com>
2022-01-18 17:02:21 +0900

    Create CodeUp_1353.java

---
+++ CodeUp/CodeUp_1353.java
@@ -0,0 +1,17 @@
+//1353 : 삼각형 출력하기 1
+
+import java.util.Scanner;
+
+public class CU1353 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
