19b1b89a1a471a7bd305c14f827515da8196f5f7
smj227b <80404571+smj227b@users.noreply.github.com>
2022-01-18 18:18:07 +0900

    Update CodeUp_1380.java

--- CodeUp/CodeUp_1380.java
+++ CodeUp/CodeUp_1380.java
@@ -14,3 +14,25 @@ public class Main {
         }
     }
 }
+
+
+
+/* 220118_손민지
+import java.util.Scanner;
+
+public class CU1380 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = sc.nextInt();
+		
+		int c = (sum<6) ? sum : 6;
+		
+		for(int num1 = 1; num1<=c; num1++) {
+			int num2 = sum - num1;
+			if(num2 != 0 && num2 <= 6) {
+				System.out.printf("%d %d\n", num1, num2);
+			}
+		}
+	}
+}
+*/
