fbb643d9d2393bab723fe3df3dea120d214c9ff9
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-18 12:56:41 +0900

    Create CodeUp_1278.java

---
+++ CodeUp_1278.java
@@ -0,0 +1,18 @@
+//1278 : 자릿수 계산
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        while(n!=0) {
+            n /= 10;
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
