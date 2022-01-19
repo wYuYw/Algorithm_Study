fccbf41f977285b29fee366f46bc00f4ab45f38b
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-20 08:27:50 +0900

    Add solution by wYuYw

--- SW_Expert_Academy/SWEA_2072.java
+++ SW_Expert_Academy/SWEA_2072.java
@@ -21,3 +21,30 @@ public class SWEA2072 {
 		}
 	}
 }
+
+
+//Solution by wYuYw
+/*
+import java.util.Scanner;
+ 
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T=sc.nextInt();
+ 
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int sum = 0;
+            for(int i = 0; i <= 9; i++) {
+                int num = sc.nextInt();
+                if (num%2 != 0) {
+                    sum += num;
+                }
+            }
+            System.out.printf("#%d %d%n", test_case, sum);
+        }
+    }
+}
+*/
