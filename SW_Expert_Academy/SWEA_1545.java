ca1a1fd36862799b6e608c843ee23494e73cb52b
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-19 09:02:02 +0900

    Update solution by wYuYw

--- SW_Expert_Academy/SWEA_1545.java
+++ SW_Expert_Academy/SWEA_1545.java
@@ -13,3 +13,21 @@ public class SWEA1545 {
 		}
 	}
 }
+
+//Solution by wYuYw
+/*
+import java.util.Scanner;
+ 
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+         
+        for (int i=N; i>=0; i--) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
+*/
