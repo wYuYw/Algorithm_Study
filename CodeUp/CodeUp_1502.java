e60f0215b5b6dcc5e8c1d8be21a5d90886266ece
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-20 10:03:45 +0900

    Add solution by wYuYw

--- CodeUp/CodeUp_1502.java
+++ CodeUp/CodeUp_1502.java
@@ -18,3 +18,30 @@ public class CU1502 {
 		}
 	}
 }
+
+
+//Solution by wYuYw
+/*
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        int result = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                result++;
+                arr[j][i] = result;
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                System.out.print(arr[i][j]+" ");
+            } System.out.println();
+        }
+    }
+}
+*/
