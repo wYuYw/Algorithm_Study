9fc05055ee081d5789a38de91cd8b329767f6050
smj227b <80404571+smj227b@users.noreply.github.com>
2022-01-20 13:24:32 +0900

    Update CodeUp_1512.java

--- CodeUp/CodeUp_1512.java
+++ CodeUp/CodeUp_1512.java
@@ -26,3 +26,38 @@ public class Main {
 
     }
 }
+
+
+/*
+// by SonMinji
+import java.util.Scanner;
+
+public class CU1512 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int [][] arr = new int[N+1][N+1];
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		arr[x][y] = 1;
+		
+		for(int i=0; i<=N; i++) {
+			for(int j=0; j<=N; j++) {
+				int xplus = x-i<0 ? (x-i)*(-1) : (x-i);
+				int yplus = y-j<0 ? (y-j)*(-1) : (y-j);
+
+				arr[i][j] = arr[x][y]+xplus+yplus;
+			}
+		}
+        
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+
