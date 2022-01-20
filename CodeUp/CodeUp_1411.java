f66de3b8d2079ee91b839622cd9f98e34e03a50c
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-20 11:07:52 +0900

    Add solution by Pro.Hong

--- CodeUp/CodeUp_1411.java
+++ CodeUp/CodeUp_1411.java
@@ -1,31 +1,18 @@
 //1411 : 빠진 카드
 
-//Solution by wYuYw
+//Solution by Pro.Hong
 
-import java.util.Arrays;
 import java.util.Scanner;
-
+ 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr1 = new int[n];
-        for(int i=0; i<n; i++) {
-            arr1[i] = i+1;
-        }
-        int[] arr2 = new int[n-1];
-        for(int i=0; i<(n-1); i++) {
-            arr2[i] = sc.nextInt();
-        }
-        Arrays.sort(arr2);
-
-        for(int i=0; i<n; i++) {
-            if(i==(n-1)) {
-                System.out.println(arr1[i]);
-                break;
-            }
-            if(arr1[i]!=arr2[i]) {
-                System.out.println(arr1[i]);
+        int N = sc.nextInt();
+        boolean[] check = new boolean[N+1];
+        for(int i=0; i<(N-1); i++) check[sc.nextInt()] = true;
+        for(int i=1; i<=N; i++) {
+            if(!check[i]) {
+                System.out.println(i);
                 break;
             }
         }
@@ -56,5 +43,39 @@ public class CU1411 {
 			c++;
 		}
 	}
+}
+*/
+
+
+//Solution by wYuYw
+/*
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr1 = new int[n];
+        for(int i=0; i<n; i++) {
+            arr1[i] = i+1;
+        }
+        int[] arr2 = new int[n-1];
+        for(int i=0; i<(n-1); i++) {
+            arr2[i] = sc.nextInt();
+        }
+        Arrays.sort(arr2);
+
+        for(int i=0; i<n; i++) {
+            if(i==(n-1)) {
+                System.out.println(arr1[i]);
+                break;
+            }
+            if(arr1[i]!=arr2[i]) {
+                System.out.println(arr1[i]);
+                break;
+            }
+        }
+    }
 }
 */
