8e3033904bfc7bdff2a3b54943fe321a37e1f598
smj227b <80404571+smj227b@users.noreply.github.com>
2022-01-20 14:06:32 +0900

    Update CodeUp_1507.java

--- CodeUp/CodeUp_1507.java
+++ CodeUp/CodeUp_1507.java
@@ -31,3 +31,40 @@ public class Main {
         System.out.println(count);
     }
 }
+
+
+
+/*
+// by SonMinji
+import java.util.Scanner;
+
+public class CU1507 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		boolean[][] arr = new boolean[100][100];
+		
+		for(int i=1; i<=4; i++) {
+			int x1 = sc.nextInt();
+			int y1 = sc.nextInt();
+			int x2 = sc.nextInt();
+			int y2 = sc.nextInt();
+			
+			for(int x=x1; x<x2; x++) {
+				for(int y=y1; y<y2; y++) {
+					arr[x][y] = true;
+				}
+			}
+		}
+		
+		int area = 0;
+		for(int x=0; x<100; x++) {
+			for(int y=0; y<100; y++) {
+				if(arr[x][y] == true) {
+					area++;
+				}
+			}
+		}
+		System.out.println(area);
+	}
+}
