6604a475da4f716e4c7a1383be8de43f4580b5cb
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-21 21:16:51 +0900

    Add solution02 by wYuYw

--- CodeUp/CodeUp_1805.java
+++ CodeUp/CodeUp_1805.java
@@ -1,7 +1,6 @@
 //1805 : 입체기동장치 생산공장
 
-//Solution by wYuYw
-
+//Solution01 by wYuYw - 2d array sort 내장함수
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -30,6 +29,27 @@ class Main {
             for(int j=0; j<2; j++) {
                 System.out.printf("%d ", arr[i][j]);
             } System.out.println();
+        }
+    }
+}
+
+
+//Solution02 by wYuYw - index counter 사용
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+		int[] arr = new int[101];
+		for(int i=0; i<n; i++) {
+			int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr[a] = b+1;
+		}
+
+        for(int i=1; i<=100; i++) {
+            if(arr[i]!=0) System.out.printf("%d %d%n", i, arr[i]-1);
         }
     }
 }
