8b660ec3277e54c240e88c2397b4b239b3d0bce4
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-21 09:54:49 +0900

    Create CodeUp_1515.java

---
+++ CodeUp/CodeUp_1515.java
@@ -0,0 +1,46 @@
+//1515 : 생명 게임 1
+
+//Solution by wYuYw
+
+import java.util.Scanner;
+
+public class Main {
+    static int numAround (int i, int j, int[][] arr) {
+        int count = 0;
+        for(int p=-1; p<=1; p++) {
+            for(int q=-1; q<=1; q++) {
+                if(p==0 && q==0) continue;
+                else if(arr[i+p][j+q]==1) count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[27][27];
+        int[][] next = new int[25][25];
+
+        for(int i=1; i<26; i++) {
+            for(int j=1; j<26; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=1; i<26; i++) {
+            for(int j=1; j<26; j++) {
+                if(numAround(i, j, arr)<=1 || numAround(i, j, arr)>=4) next[i-1][j-1] = 0;
+                else if(numAround(i, j, arr)==2 || numAround(i, j, arr)==3) {
+                    if(arr[i][j]==1) next[i-1][j-1] = 1;
+                    if(numAround(i, j, arr)==3) next[i-1][j-1] = 1;
+                }
+            }
+        }
+        
+        for(int p=0; p<25; p++) {
+            for(int q=0; q<25; q++) {
+                System.out.printf("%d ", next[p][q]);
+            } System.out.println();
+        }
+    }
+}
