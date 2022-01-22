f59ff667361449852033ef5fe4a40864b1937011
smj227b <80404571+smj227b@users.noreply.github.com>
2022-01-22 10:52:25 +0900

    Update CodeUp_1282.java

--- CodeUp/CodeUp_1282.java
+++ CodeUp/CodeUp_1282.java
@@ -11,7 +11,7 @@ public class CU1282 {
 		// 문제에서 (0<k<n<=2^31)라고 주어졌으니까 int n이 아니라 long n
 		// k는 n보다 작다고 했으니 int여도 괜찮지만 long(n에 해당)-int(i*i에 해당)가 long타입이라 long k로 바꿔줘야 함
 		// int는 4바이트니까 4*8 = 32 -1(부호비트) 해서 2^31 - 1 까지 넣을 수 있다
-		long n = sc.nextInt();
+		long n = sc.nextLong();
 		
 		for(int i=1; ; i++) {
 			if(n<=(i+1)*(i+1) && n>=i*i) {
@@ -22,3 +22,26 @@ public class CU1282 {
 		}
 	}	
 }
+
+// by SonMinji
+// 반복 횟수를 모르겠을 때는 while문에서 break를 사용하는 게 더 나을 것 같다
+/*
+import java.util.Scanner;
+
+public class CU1282re {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextLong();
+		int i = 1;
+		while(i>0) {
+			if(n<=(i+1)*(i+1) && n>=i*i) {
+				long k = n - i*i;
+				System.out.println(k+" "+i);
+				break;
+			}
+			i++;
+		}
+	}	
+}
+*/
