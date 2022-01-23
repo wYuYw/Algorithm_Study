e9cb814d0709c9a5ff0074b9a2b0dd68791d80d6
smj227b <80404571+smj227b@users.noreply.github.com>
2022-01-23 22:58:19 +0900

    Update CodeUp_1511.java

--- CodeUp/CodeUp_1511.java
+++ CodeUp/CodeUp_1511.java
@@ -26,3 +26,80 @@ public class Main {
         System.out.println(value);
     }
 }
+
+
+// by SonMinji_220123
+/*
+import java.util.Scanner;
+
+public class CU1511 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int[][] arr = new int[N][N];
+		int num=1;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				arr[i][j] = num++;
+				// System.out.print(arr[i][j]);
+			}
+			// System.out.println();
+		}
+		
+		int sum = 0;
+		for(int r=0; r<N; r++) {
+			if(r==0 || r==N-1) {
+				for(int c=0; c<N; c++) {
+					sum += arr[r][c];
+				}	
+			} else {
+				// 조건이 꼭 c++일 필요는 없지! 고정관념 버리자
+				for(int c=0; c<N; c=c+N-1) {
+					sum += arr[r][c];
+				}
+			}
+		}
+		System.out.println(sum);
+	}
+}
+*/
+
+
+// Solution2 by SonMinji_220123
+/*
+import java.util.Scanner;
+
+public class CU1511re {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int[][] arr = new int[N][N];
+		int num=1;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				arr[i][j] = num++;
+				// System.out.print(arr[i][j]);
+			}
+			// System.out.println();
+		}
+		
+        // 테두리말고 나머지 값 0으로 바꾸기
+		for(int r=1; r<N-1; r++) {
+			for(int c=1; c<N-1; c++) {
+				arr[r][c] = 0;
+			}
+		}
+		
+		int sum = 0;
+        // 배열의 모든 값 더하기
+		for(int r=0; r<N; r++) {
+			for(int c=0; c<N; c++) {
+				sum += arr[r][c];
+			}
+		}
+		System.out.println(sum);
+	}
+}
+*/
