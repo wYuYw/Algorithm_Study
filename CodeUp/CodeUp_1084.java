1c86cc64bd8c65a1a560b89091d7c3e7afb3ae03
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-23 16:22:03 +0900

    Add solution by wYuYw

--- CodeUp/CodeUp_1084.java
+++ CodeUp/CodeUp_1084.java
@@ -37,3 +37,33 @@ public class CU1084 {
 		bf.close();
 	}
 }
+
+
+//Solution by wYuYw - BufferedReader & BufferedWriter
+/*
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] color = br.readLine().split(" ");
+		
+		int num = 0;
+		for(int i=0; i<Integer.valueOf(color[0]); i++) {
+			for(int j=0; j<Integer.valueOf(color[1]); j++) {
+				for(int k=0; k<Integer.valueOf(color[2]); k++) {
+					bw.write(i + " " + j + " " + k + "\n");
+					num++;
+				}
+			}
+		}
+		bw.write(String.valueOf(num));
+		bw.flush();
+	}
+}
+*/
