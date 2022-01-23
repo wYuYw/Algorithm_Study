3732cc4806ed75ee91451fd93df1209fbee00dfc
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-23 16:32:15 +0900

    Add solution by wYuYw

--- CodeUp/CodeUp_1409.java
+++ CodeUp/CodeUp_1409.java
@@ -16,3 +16,25 @@ public class CU1409 {
 		System.out.println(arr[k-1]);
 	}
 }
+
+
+//Solution by wYuYw
+/*
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] nums = br.readLine().split(" ");
+		int k = Integer.parseInt(br.readLine());
+		
+		bw.write(nums[k-1]);
+		bw.flush();
+	}
+}
+*/
