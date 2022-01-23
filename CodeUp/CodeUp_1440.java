fd7887635acac29107d5dbc4f9a45848a18ab6fc
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-23 17:05:02 +0900

    Add solution by wYuYw

--- CodeUp/CodeUp_1440.java
+++ CodeUp/CodeUp_1440.java
@@ -29,3 +29,43 @@ public class CU1440 {
 		}
 	}
 }
+
+
+//Solution by wYuYw
+/*
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int k = Integer.parseInt(br.readLine());
+		String[] nums = br.readLine().split(" ");
+    int[] numsInt = new int[k];
+		
+		for(int i=0; i<k; i++) {
+			numsInt[i] = Integer.parseInt(nums[i]);
+		}
+		
+		for(int i=0; i<k; i++) {
+			bw.write(i+1 + ": ");
+			for(int j=0; j<k; j++) {
+				if(i==j) continue;
+				else {
+					if(numsInt[i] == numsInt[j]) {
+						bw.write("= ");
+					} else {
+						bw.write(numsInt[i] > numsInt[j] ? "> " : "< ");
+					}
+				}
+			}
+			bw.write("\n");
+		}
+		bw.flush();
+	}
+}
+*/
