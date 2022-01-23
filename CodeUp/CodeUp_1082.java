05fbd0eef4b70f4aaaea767a57521daede570ec6
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-23 15:49:25 +0900

    Update solution by wYuYw

--- CodeUp/CodeUp_1082.java
+++ CodeUp/CodeUp_1082.java
@@ -25,3 +25,19 @@ public class CU1082 {
 		}
 	}
 }
+
+
+//Solution by wYuYw
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(16);
+		
+		for(int i=1; i<16; i++) {
+			System.out.printf("%X*%X=%X%n", n, i, n*i);
+		}
+	}
+}
