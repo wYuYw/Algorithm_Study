f5589c1181a498a288290007c52c5f3513e0ee25
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-01-24 22:13:54 +0900

    Add solution02 by wYuYw

--- SW_Expert_Academy/SWEA_1288.java
+++ SW_Expert_Academy/SWEA_1288.java
@@ -1,6 +1,6 @@
 // 1288. 새로운 불면증 치료법
 
-// Solution by wYuYw
+// Solution01 by wYuYw
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -45,3 +45,47 @@ class Solution
         bw.close();
 	}
 }
+
+
+// Solution02 by wYuYw - Use StringBuilder instead of BufferedWriter
+/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+    
+class Solution
+{
+	public static void main(String args[]) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            String N = br.readLine();
+            int M = Integer.parseInt(N);
+            boolean[] arr = new boolean[10];
+            int cnt = 0;
+            int num = 1;
+            
+            while(true) {
+                for (int i = 0; i <N.length() ; i++) {
+                    int temp = Integer.parseInt(N.substring(i, i+1));
+            		if(arr[temp]==false) {
+                    	arr[temp] = true;
+                        cnt++;
+                    }
+       			}
+                if(cnt==10) break;
+                else {
+                    num++;
+                    N = Integer.toString(num*M);
+                }
+            }
+            sb.append("#"+test_case+" "+N+"\n");
+		}
+        System.out.print(sb.toString());
+	}
+}
+*/
