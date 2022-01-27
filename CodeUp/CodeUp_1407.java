d19046354a8fe201b76124788778c9dd7997e967
smj227b <80404571+smj227b@users.noreply.github.com>
2022-01-27 23:31:00 +0900

    Create CodeUp_1407.java

---
+++ CodeUp/CodeUp_1407.java
@@ -0,0 +1,14 @@
+// 1407 : 문자열 출력하기 1
+
+// by SonMinji_220127
+
+import java.util.Scanner;
+
+public class CU1407 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		
+		System.out.println(str.replace(" ", ""));
+	}
+}
