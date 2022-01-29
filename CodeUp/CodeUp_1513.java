8009c6386fdf1a4fe7e42411548bb1e068ad1b89
smj227b <80404571+smj227b@users.noreply.github.com>
2022-01-29 23:34:46 +0900

    Update CodeUp_1513.java by smj227b
    Update CodeUp_1513.java by smj227b

--- CodeUp/CodeUp_1513.java
+++ CodeUp/CodeUp_1513.java
@@ -38,3 +38,46 @@ public class Main {
         }
     }
 }
+
+
+// by SonMinji_220129
+/*
+import java.util.Scanner;
+
+public class CU1513 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int [][] arr = new int[n][n];
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				arr[i][j] = 0;
+			}
+		}
+		
+		int value = 1;
+		for(int c=1; c<=n; c++) {
+			for(int i=n-1; i>=0; i--) {
+				for(int j=n-1-i; j<n; j++) {
+					if(c % 2 == 1 && arr[i][j] == 0) {
+						arr[i][j] = value++;
+						break;
+					} else if(c % 2 == 0 && arr[j][i] == 0) {
+						arr[j][i] = value++;
+						break;
+					}
+				}
+			}
+		}
+
+		for(int r=0; r<n; r++) {
+			for(int c=0; c<n; c++) {
+				System.out.print(arr[r][c]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+*/
