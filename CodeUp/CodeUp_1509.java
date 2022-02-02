2d1fdbdfd6702de23ec8575d343476828475dd8f
smj227b <80404571+smj227b@users.noreply.github.com>
2022-02-02 23:21:24 +0900

    Update CodeUp_1509.java by smj227b
    Update CodeUp_1509.java by smj227b

--- CodeUp/CodeUp_1509.java
+++ CodeUp/CodeUp_1509.java
@@ -34,3 +34,52 @@ public class Main {
         }
     }
 }
+
+
+
+// by SonMinji_220202
+/*
+import java.util.Scanner;
+
+public class CU1509 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[][] arr = new int[10][10];
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		int[] horseArr = new int[10];
+		for(int h=0; h<10; h++) {
+			horseArr[h] = sc.nextInt();
+		}
+		
+		String[] resultArr = new String[10];
+		for(int c=0; c<10; c++) {
+			resultArr[c] = "safe";
+		}
+		
+		for(int c=0; c<10; c++) {
+			for(int r=10-1; r>=0; r--) {
+				if(arr[r][c] > 0) {
+					resultArr[c] = "crash";
+					break;
+				} else if(arr[r][c] < 0) {
+					resultArr[c] = "fall";
+					break;
+				}
+			}
+		}
+		
+		for(int c=0; c<10; c++) {
+			if(horseArr[c] == 1) {
+				System.out.printf("%d %s\n", c+1, resultArr[c]);
+			}
+		}
+
+	}
+}
+*/
