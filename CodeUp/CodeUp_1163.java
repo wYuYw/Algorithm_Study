f765e7abf161b7c2ab666ac2698d3eb57803b9eb
smj227b <80404571+smj227b@users.noreply.github.com>
2022-02-03 00:17:33 +0900

    Create CodeUp_1163.java by smj227b
    Create CodeUp_1163.java by smj227b

---
+++ CodeUp/CodeUp_1163.java
@@ -0,0 +1,22 @@
+// 1163 : 당신의 사주를 봐 드립니다 2
+
+// by SonMinji_220202
+
+import java.util.Scanner;
+
+public class CU1163 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int year = sc.nextInt();
+		int month = sc.nextInt();
+		int day = sc.nextInt();
+		
+		int n = year + month + day;
+		if((n / 100) % 2 == 0) {
+			System.out.println("대박");
+		} else {
+			System.out.println("그럭저럭");
+		}
+	}
+}
