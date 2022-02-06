5de45b5942b6ea1d0d34ba916aa83a3a48479ce9
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-02-06 14:41:14 +0900

    Add solution by wYuYw

--- Baekjoon/Implementation/Baekjoon_2490.py
+++ Baekjoon/Implementation/Baekjoon_2490.py
@@ -1,3 +1,10 @@
 # Baekjoon_2490
 
 # Solution by wYuYw
+
+data = {0:"D", 1:"C", 2:"B", 3:"A", 4:"E"}
+
+for i in range(3):
+    play = input().split()
+    val = play.count("1")
+    print(data[val])
