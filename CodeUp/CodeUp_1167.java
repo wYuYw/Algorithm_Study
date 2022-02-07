130f9a88f11ad9d2621330daf332aedf9a6fdc93
smj227b <80404571+smj227b@users.noreply.github.com>
2022-02-07 20:48:52 +0900

    Update CodeUp_1167.java by smj227b
    Update CodeUp_1167.java by smj227b

--- CodeUp/CodeUp_1167.java
+++ CodeUp/CodeUp_1167.java
@@ -24,3 +24,32 @@ public class CU1167 {
 		System.out.print(arr[1]);
 	}
 }
+
+
+// solution2 by SonMinji_220207
+// j변수가 없어도 돼서 조금 다르게 다시 작성
+/*
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[3];
+		for(int v=0; v<3; v++) {
+			arr[v] = sc.nextInt();
+		}
+
+		for(int i=0; i<3-1; i++) {
+			for(int k=0; k<3-1-i; k++) {
+				if(arr[k] > arr[k+1]) {
+					int temp = arr[k];
+					arr[k] = arr[k+1];
+					arr[k+1] = temp;
+				}
+			}
+		}
+		System.out.print(arr[1]);
+	}
+}
+*/
