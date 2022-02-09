64333577a50de57a0578b2c1d7c275e7e837fbf5
smj227b <80404571+smj227b@users.noreply.github.com>
2022-02-09 22:25:59 +0900

    Create CodeUp_1441.java by smj227b
    Create CodeUp_1441.java by smj227b

---
+++ CodeUp/CodeUp_1441.java
@@ -0,0 +1,30 @@
+// 1441 : 버블 정렬
+
+// by SonMinji_220209
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i=arr.length-1; i>=1; i--) {
+			for(int j=0; j<i; j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		for (int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
