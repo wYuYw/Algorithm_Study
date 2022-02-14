b84780c9a8b3019648cc63bda76b3854794dbad7
smj227b <80404571+smj227b@users.noreply.github.com>
2022-02-14 23:43:15 +0900

    Update SWEA_1989.java by smj227b
    Update SWEA_1989.java by smj227b

--- SW_Expert_Academy/SWEA_1989.java
+++ SW_Expert_Academy/SWEA_1989.java
@@ -57,3 +57,61 @@ class Solution
 	}
 }
 */
+
+
+
+// Solution01 by SonMinji_220214 - for문 사용하여 변수에 저장하여 검사
+/*
+import java.util.Scanner;
+
+public class SWEA_D2_1989 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int tcNum=1; tcNum<=T; tcNum++) {
+			String str = sc.next();
+			String reverseStr = "";
+			int result = 0;
+			for(int i=str.length()-1; i>=0; i--) {
+				reverseStr += str.charAt(i);
+			}
+			//System.out.println(reverseStr);
+			if(str.equals(reverseStr)) {
+				result = 1;
+			}
+			System.out.printf("#%d %d\n", tcNum, result);
+		}
+
+	}
+}
+*/
+
+
+// Solution02 by SonMinji_220214 - for문 사용하며 charAt(idx)로 바로 검사
+/*
+import java.util.Scanner;
+
+public class SWEA_D2_1989 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int tcNum=1; tcNum<=T; tcNum++) {
+
+			String str = sc.next();
+			int result = 1;
+			for(int i=0; i<str.length()/2; i++) {
+				if(str.charAt(i) != str.charAt(str.length()-1-i)) {
+					result = 0;
+					break;
+				}
+			}
+			System.out.printf("#%d %d\n", tcNum, result);
+		}
+		
+	}
+}
+*/
