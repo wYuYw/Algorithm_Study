e1f3a286916cf840bc4cabf594aa136ea3fdc0e5
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-02-16 21:11:05 +0900

    Add solution by wYuYw

--- SW_Expert_Academy/SWEA_2805.java
+++ SW_Expert_Academy/SWEA_2805.java
@@ -33,3 +33,42 @@ public class SWEA_D3_2805 {
 		}
 	}
 }
+
+
+// Solution by wYuYw
+
+import java.util.Scanner;
+ 
+class Solution {
+    public static void main(String args[]) throws Exception {
+         
+        Scanner sc = new Scanner(System.in);
+        int T=sc.nextInt();
+ 
+        for(int test_case = 1; test_case <= T; test_case++) {
+            int result = 0;
+            int n = sc.nextInt();
+            int[][] farm = new int[n][n];
+             
+            // Form a farm
+            for ( int i = 0; i < n; i++ ) {
+                String temp = sc.next();
+                for ( int j = 0; j < n; j++ ) {
+                    farm[i][j] = Character.getNumericValue(temp.charAt(j));
+                }
+            }
+            // Middle area
+            for ( int i = 0; i < n; i++ ) result += farm[n/2][i];
+            for ( int i = 0; i < n/2; i++ ) {
+                for ( int j = n/2 - i; j <= n/2 + i; j++ ) {
+                    // Upper area
+                    result += farm[i][j];
+                    // Lower area
+                    result += farm[n-i-1][j];
+                }
+            }
+ 
+            System.out.printf("#%d %d%n", test_case, result);
+        }
+    }
+}
