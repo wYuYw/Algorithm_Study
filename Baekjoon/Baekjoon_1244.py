96503ffd63d4bc1a050ae6b05a27c87d6650cac2
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-02-17 14:00:07 +0900

    Add solution by wYuYw

--- Baekjoon/Baekjoon_1244.py
+++ Baekjoon/Baekjoon_1244.py
@@ -1,1 +1,29 @@
 # Baekjoon_1244 스위치 켜고 끄기
+
+# Solution by wYuYw
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+switch = [-1] + list(map(int, input().split()))
+students = int(input())
+for _ in range(students):
+    gender, num = map(int, input().split())
+    # male
+    if gender == 1:
+        for i in range(num, N+1, num):
+            switch[i] ^= 1
+    # female
+    else:
+        switch[num] ^= 1
+        for k in range(N//2):
+            if num + k > N or num - k < 1 : break
+            if switch[num + k] == switch[num - k]:
+                switch[num+k] ^= 1
+                switch[num-k] ^= 1
+            else:
+                break
+
+for i in range(1, N+1, 20):
+    print(*switch[i:i+20])
