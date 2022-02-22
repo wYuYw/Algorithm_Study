5967169cbb068cc9cc95e7dec68517107c2018dc
smj227b <80404571+smj227b@users.noreply.github.com>
2022-02-23 00:42:52 +0900

    Update SWEA_1961.java by smj227b

--- SW_Expert_Academy/SWEA_1961.java
+++ SW_Expert_Academy/SWEA_1961.java
@@ -42,3 +42,66 @@ class Solution {
 		return result;
 	}
 }
+
+
+
+// by SonMinji_220222
+/*
+import java.util.Scanner;
+
+public class SWEA_D2_1961 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tcNum = 1; tcNum <= T; tcNum++) {
+			int N = sc.nextInt();
+			int[][] arr = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+
+			StringBuilder sb90 = new StringBuilder();
+			for (int c = 0; c < N; c++) {
+				for (int r = N - 1; r >= 0; r--) {
+					sb90.append(arr[r][c]);
+				}
+			}
+			StringBuilder sb180 = new StringBuilder();
+			for (int r = N - 1; r >= 0; r--) {
+				for (int c = N - 1; c >= 0; c--) {
+					sb180.append(arr[r][c]);
+				}
+			}
+			StringBuilder sb270 = new StringBuilder();
+			for (int c = N - 1; c >= 0; c--) {
+				for (int r = 0; r < N; r++) {
+					sb270.append(arr[r][c]);
+				}
+			}
+
+			String str90 = sb90.toString();
+			String str180 = sb180.toString();
+			String str270 = sb270.toString();
+			
+			String[][] strArr = new String[N][3];
+			System.out.println("#" + tcNum);
+			for(int r=0; r<N; r++) {
+				for(int c=0; c<3; c++) {
+					if(c==0) {
+						strArr[r][c] = str90.substring(N*r, N*(r+1));
+					} else if(c==1) {
+						strArr[r][c] = str180.substring(N*r, N*(r+1));
+					} else {
+						strArr[r][c] = str270.substring(N*r, N*(r+1));
+					}
+					System.out.print(strArr[r][c]+" ");
+				}
+				System.out.println();
+			}
+
+		}
+	}
+}
+*/
