66fa0cb4e32ed2e0230b63cf0041488d945c8bc1
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-02-22 10:40:29 +0900

    Add original solution by wYuYw

--- Baekjoon/DP/Baekjoon_11025.py
+++ Baekjoon/DP/Baekjoon_11025.py
@@ -1,6 +1,18 @@
 # Baekjoon_11025 요세푸스 문제 3
 
-# Solution by wYuYw
+# Solution_01 by wYuYw
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+result = 0
+for i in range(1, n+1):
+    result = (result + k - 1) % i + 1
+print(result)
+
+
+# Solution_02 by wYuYw - Simplify
 
 import sys
 input = sys.stdin.readline
