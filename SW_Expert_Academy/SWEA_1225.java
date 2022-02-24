7ec991a80c8d62d9ee5cd42c2f5201b98d08b084
smj227b <80404571+smj227b@users.noreply.github.com>
2022-02-24 23:37:31 +0900

    Update SWEA_1225.java by smj227b

--- SW_Expert_Academy/SWEA_1225.java
+++ SW_Expert_Academy/SWEA_1225.java
@@ -32,3 +32,94 @@ public class Solution {
         }
     }
 }
+
+
+
+// Solution01 by SonMinji_220224 - 큐(Queue)
+/*
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+ 
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int tc=1; tc<=10; tc++) {
+            int tcNum = sc.nextInt();
+             
+            Queue<Integer> code = new LinkedList<Integer>();
+            for(int i=0; i<8; i++) {
+                code.add(sc.nextInt());
+            }
+             
+            out: while(true) {
+                for(int n=1; n<=5; n++) {
+                    if(code.element()-n <= 0) {
+                        code.add(0);
+                        code.remove();
+                        break out;
+                    }
+                    code.add(code.remove()-n);
+                }
+            }
+             
+            StringBuilder sb = new StringBuilder();
+            while(!code.isEmpty()) {
+                sb.append(" "+code.remove());
+            }
+            String result = sb.toString();
+            System.out.printf("#%d%s\n", tcNum, result);
+        }
+    }
+}
+*/
+
+
+// Solution02 by SonMinji_220224 - 모듈러 연산(%) 
+/*
+import java.util.Scanner;
+ 
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int tc=1; tc<=10; tc++) {
+            int tcNum = sc.nextInt();
+            int[] code = new int[8];
+            for(int i=0; i<8; i++) {
+                code[i] = sc.nextInt();
+            }
+             
+            int n=0;
+            int idx = -1;
+            out: while(true) {
+                for(int i=0; i<8; i++) {
+                    ++n;
+                    n = n % 5 != 0 ? n % 5 : 5;
+                    if(code[i] - n <= 0) {
+                        code[i] = 0;
+                        idx = i;
+                        break out;
+                    }
+                    code[i] = code[i] - n;
+                }
+            }
+             
+            StringBuilder sb = new StringBuilder();
+            // 방법 1
+            for(int i=idx+1; i<8; i++) {
+                sb.append(" "+code[i]);
+            }
+            for(int i=0; i<=idx; i++) {
+                sb.append(" "+code[i]);
+            }
+            // 방법 2
+            //int fakeIdx = (idx+1)+8;
+            //for(int i=idx+1; i<fakeIdx; i++) {
+            //    sb.append(" "+code[i%8]);
+            //}
+            String result = sb.toString();
+            System.out.printf("#%d%s\n", tcNum, result);
+        }
+    }
+}
+*/
