580278c32ed82736c398c9f702a40c26628a3579
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-02-25 14:06:09 +0900

    Update Leetcode_819.py

--- Leetcode/String/Leetcode_819.py
+++ Leetcode/String/Leetcode_819.py
@@ -5,6 +5,7 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         words = [word for word in re.sub('[^\w]', ' ', paragraph).lower().split()
+        # words = [word for word in re.sub('r[^\w]', ' ', paragraph).lower().split() <- r이 붙을 경우, 문자를 있는 그대로; \는 백슬레쉬로
                  if word not in banned]
         
         counter = collections.Counter(words)
