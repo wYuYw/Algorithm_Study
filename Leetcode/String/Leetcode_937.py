db519b0455a0517370ccc29c0a062b45ba878d38
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-02-25 13:28:23 +0900

    Create Leetcode_937.py

---
+++ Leetcode/String/Leetcode_937.py
@@ -0,0 +1,15 @@
+# Leetcode_937. Reorder Data in Log Files
+
+# Solution by wYuYw - Using Lambda to sort
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+                
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return letters + digits
