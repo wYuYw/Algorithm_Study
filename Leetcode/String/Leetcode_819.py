286120cd2d02e1c65ee1fc602852ff076a9d05a7
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-02-25 13:58:40 +0900

    Create Leetcode_819.py

---
+++ Leetcode/String/Leetcode_819.py
@@ -0,0 +1,11 @@
+# Leetcode_819. Most Common Word
+
+# Solution by wYuYw
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub('[^\w]', ' ', paragraph).lower().split()
+                 if word not in banned]
+        
+        counter = collections.Counter(words)
+        return counter.most_common(1)[0][0]
