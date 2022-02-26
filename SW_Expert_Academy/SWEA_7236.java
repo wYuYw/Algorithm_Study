3196e7ff935dae4cc6cd809484c1e68972c84799
smj227b <80404571+smj227b@users.noreply.github.com>
2022-02-26 14:19:55 +0900

    Update SWEA_7236.java by smj227b

--- SW_Expert_Academy/SWEA_7236.java
+++ SW_Expert_Academy/SWEA_7236.java
@@ -67,3 +67,51 @@ class Solution {
 		}
 	}
 }
+
+
+
+// by SonMinji_220226
+
+import java.util.Scanner;
+
+public class SWEA_D3_7236 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int tcNum=1; tcNum<=T; tcNum++) {
+			int N = sc.nextInt();
+			char[][] map = new char[N+2][N+2];
+			for(int i=1; i<=N; i++) {
+				for(int j=1; j<=N; j++) {
+					map[i][j] = sc.next().charAt(0);
+				}
+			}
+            
+			int max = 0;
+			for(int r=1; r<=N; r++) {
+				for(int c=1; c<=N; c++) {
+					
+					if(map[r][c] == 'W') {
+						int sum = -1;
+						for(int mr=r-1; mr<=r+1; mr++) {
+							for(int mc=c-1; mc<=c+1; mc++) {
+								if(map[mr][mc] == 'W') {
+									sum++;
+								}
+							}
+						}
+						if(sum == 0) {
+							sum = 1;
+						}
+						max = Math.max(max, sum);
+					}
+                    
+				}
+			}
+			
+			System.out.printf("#%d %d\n", tcNum, max);
+		}	
+		
+	}
+}
