b960dce38d8ee8048e2538436749b870636c5aea
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-03-01 07:35:06 +0900

    Add solution by using bucket sort

--- Leetcode/Heapq/Leetcode_347.py
+++ Leetcode/Heapq/Leetcode_347.py
@@ -25,3 +25,23 @@ class Solution:
             topk.append(heapq.heappop(freqs_heap)[1])
             
         return topk
+
+    
+# Solution_03 by wYuYw - Using bucket sort
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for _ in range(len(nums) + 1)]
+        
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+            
+        res = []
+        for i in reversed(range(len(freq))):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
