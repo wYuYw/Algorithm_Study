b7b9ca7de87ae492a825f15e507968bee1356e9d
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-02-28 15:42:47 +0900

    Create Leetcode_234.py

---
+++ Leetcode/Two_Pointer/Runner/Leetcode_234.py
@@ -0,0 +1,24 @@
+# Leetcode_234. Palindrome Linked List
+
+# Solution by wYuYw
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        rev = None
+        slow = fast = head
+        # 런너를 이용해 역순 연결 리스트 구성
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+            
+        # is palindrome?
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+        return not rev
