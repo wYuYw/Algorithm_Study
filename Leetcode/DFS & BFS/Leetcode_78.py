bc950018750c0b5e9cfe37c5487b9f71c7da2563
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-03-03 07:28:29 +0900

    Add solution_03 by wYuYw

--- Leetcode/DFS & BFS/Leetcode_78.py
+++ Leetcode/DFS & BFS/Leetcode_78.py
@@ -42,3 +42,15 @@ class Solution:
 
         dfs(0)
         return res
+    
+    
+# Solution_03 by wYuYw - Using itertools
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        for i in range(len(nums)+1):
+            res += list(itertools.combinations(nums, i))
+            
+        return res
