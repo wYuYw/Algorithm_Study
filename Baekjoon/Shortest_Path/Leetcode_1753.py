2e902e0b39c45cd7dd5ef2e4ef81ef5f88569a96
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-03-08 17:46:06 +0900

    Add faster solution

--- Baekjoon/Shortest_Path/Leetcode_1753.py
+++ Baekjoon/Shortest_Path/Leetcode_1753.py
@@ -1,6 +1,6 @@
 # Leetcode_1753 최단경로
 
-# Solution by wYuYw
+# Solution_01 by wYuYw
 
 from collections import defaultdict
 import heapq
@@ -30,3 +30,32 @@ for i in range(1, V + 1):
         print("INF")
     else:
         print(dist[i])
+
+        
+# Solution_02 - Faster code
+
+import heapq as hq
+import sys
+input = sys.stdin.readline
+V, E = map(int, input().split())
+S = int(input())
+graph, dist, INF = [[] for _ in range(V + 1)], [int(1e10)] * (V + 1), int(1e10)
+for _ in range(E):
+    s, e, d = map(int, input().split())
+    graph[s].append((e, d))
+#input end
+def dijkstra(start: int, dist: list):
+    q = [(0, start)]
+    dist[start] = 0
+    while q:
+        cost, cur = hq.heappop(q)
+        if dist[cur] < cost: continue
+        for nxt, d in graph[cur]:
+            nxt_cost = cost + d
+            if dist[nxt] > nxt_cost:
+                dist[nxt] = nxt_cost
+                hq.heappush(q, (nxt_cost, nxt))
+
+dijkstra(S, dist)
+for i in range(1, V + 1):
+    print("INF" if dist[i] == INF else dist[i])
