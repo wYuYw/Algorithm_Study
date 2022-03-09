fd358c366e031ff7643a81a295b120f886b620ca
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-03-10 08:45:12 +0900

    Create Leetcode_406.py

---
+++ Leetcode/Greedy/Leetcode_406.py
@@ -0,0 +1,18 @@
+# Leetcode_406. Queue Reconstruction by Height
+
+# Solution - Using priority queue
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        heap = []
+        # 키 역순, 인덱스 삽입
+        for person in people:
+            heapq.heappush(heap, (-person[0], person[1]))
+            
+        result = []
+        # 키 역순, 인덱스 추출
+        while heap:
+            person = heapq.heappop(heap)
+            result.insert(person[1], [-person[0], person[1]])
+            
+        return result
