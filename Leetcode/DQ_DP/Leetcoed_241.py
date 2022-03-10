295c3ae75e23fd802302edea07fc79b5524ab895
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-03-11 07:19:10 +0900

    Create Leetcoed_241.py

---
+++ Leetcode/DQ_DP/Leetcoed_241.py
@@ -0,0 +1,25 @@
+# Leetcoed_241. Different Ways to Add Parentheses
+
+# Solution - Using DQ
+
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        def compute(left, right, op):
+            results = []
+            
+            for l in left:
+                for r in right:
+                    results.append(eval(str(l) + op + str(r)))
+            return results
+        
+        if expression.isdigit():
+            return [int(expression)]
+        
+        results = []
+        for index, value in enumerate(expression):
+            if value in "-+*":
+                left = self.diffWaysToCompute(expression[:index])
+                right = self.diffWaysToCompute(expression[index+1:])
+                
+                results.extend(compute(left, right, value))
+        return results
