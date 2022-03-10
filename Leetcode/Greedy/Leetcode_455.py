8a81bc747f90616cb9bae6db570b995dcbdfc738
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-03-10 21:48:42 +0900

    Create Leetcode_455.py

---
+++ Leetcode/Greedy/Leetcode_455.py
@@ -0,0 +1,17 @@
+# Leetcode_455. Assign Cookies
+
+# Solution by wYuYw
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        
+        child_i, cookie_j = 0, 0
+        # 만족하지 못할 때까지 그리디 진행
+        while child_i < len(g) and cookie_j < len(s):
+            if s[cookie_j] >= g[child_i]:
+                child_i += 1
+            cookie_j += 1
+            
+        return child_i
