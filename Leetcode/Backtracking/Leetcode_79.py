b275f5fad6bfe84f4a46b5254fcc1e2db815933e
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-03-16 07:07:35 +0900

    Create Leetcode_79.py

---
+++ Leetcode/Backtracking/Leetcode_79.py
@@ -0,0 +1,32 @@
+# Leetcode_79. Word Search
+
+# Solution - Brute Force, DFS, Backtracking
+# Time Complexity ~ O(n * m * dfs), O(dfs) ~ 4^len(word)
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        path = set() # to make sure not to revisit the previous path
+        
+        def dfs(r, c, i): # (current r, current c, current character)
+            if i == len(word):
+                return True
+            if (r < 0 or c < 0 or
+                r >= rows or c >= cols or
+                word[i] != board[r][c] or
+                (r, c) in path):
+                return False
+            
+            path.add((r, c))
+            res = (dfs(r + 1, c, i + 1) or
+                   dfs(r - 1, c, i + 1) or
+                   dfs(r, c + 1, i + 1) or
+                   dfs(r, c - 1, i + 1))
+            
+            path.remove((r, c))
+            return res
+        
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0): return True
+        return False
