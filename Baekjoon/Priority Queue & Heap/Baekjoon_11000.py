d1979bdb1b42e88e571fa1ea4237d5d5f9649a91
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-03-17 07:01:31 +0900

    Create Baekjoon_11000.py

---
+++ Baekjoon/Priority Queue & Heap/Baekjoon_11000.py
@@ -0,0 +1,20 @@
+# Baekjoon_11000 강의실 배정
+
+# Solution by wYuYw
+
+import heapq
+
+n = int(input()) # 1 ≤ N ≤ 200,000
+times = [tuple(map(int, input().split())) for _ in range(n)]
+times.sort()
+
+classrooms = []
+heapq.heappush(classrooms, times[0][1])
+for i in range(1, n):
+    if times[i][0] < classrooms[0]:
+        heapq.heappush(classrooms, times[i][1])
+    else:
+        heapq.heappop(classrooms)
+        heapq.heappush(classrooms, times[i][1])
+
+print(len(classrooms))
