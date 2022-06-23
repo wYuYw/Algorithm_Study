07ec4c0756041353247ebcc9ab7d4508110651f6
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-06-23 17:51:57 +0900

    Create Leetcode_20.py

---
+++ Leetcode/Stack/Leetcode_20.py
@@ -0,0 +1,18 @@
+# Leetcode_20. Valid Parentheses
+
+# Solution by wYuYw
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        table = {')' : '(',
+                 '}' : '{',
+                 ']' : '['}
+        
+        for char in s:
+            if char not in table:
+                stack.append(char)
+            elif not stack or table[char] != stack.pop():
+                return False
+        
+        return len(stack) == 0
