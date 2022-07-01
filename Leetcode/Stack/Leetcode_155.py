a350dce27deeb48090a5a73bcf964d796b5970fb
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-07-01 12:30:15 +0900

    Create Leetcode_155.py

---
+++ Leetcode/Stack/Leetcode_155.py
@@ -0,0 +1,45 @@
+# Leetcode_155. Min Stack
+
+# Solution_01 by wYuYw - only using stack
+# (Runtime, Memory) = (1346 ms, 17.7 MB)
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+      
+
+# Solution_02 by wYuYw - also using stack that accumulates min_values
+# (Runtime, Memory) = (63 ms, 18.2 MB)
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.min_stack.append(min(val, self.min_stack[-1] if self.min_stack else val))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
