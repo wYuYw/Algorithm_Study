fcd3877520498f0de7fba33a54e2156d8cf12752
wYuYw <83063691+wYuYw@users.noreply.github.com>
2022-07-06 07:46:46 +0900

    Create Leetcode_746.py

---
+++ Leetcode/DFS & BFS/Leetcode_746.py
@@ -0,0 +1,11 @@
+# Leetcode_746. Min Cost Climbing Stairs
+
+# Solution by wYuYw
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = [0] * (len(cost) + 1)
+        for i in range(2, len(dp)):
+            dp[i] = min(cost[i - 1] + dp[i - 1], cost[i - 2] + dp[i - 2])
+        
+        return dp[-1]
