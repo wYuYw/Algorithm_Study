57b3f4043cff306e210cbeef50c9a302be42f9b3
wYuYw <83063691+wYuYw@users.noreply.github.com>
2023-01-11 21:45:50 +0900

    Create Baekjoon_1000.js

---
+++ Baekjoon/Basic/Baekjoon_1000.js
@@ -0,0 +1,9 @@
+// Baekjoon_1000 A+B
+
+// Solution by wYuYw
+
+let fs = require('fs');
+let arr = fs.readFileSync('/dev/stdin').toString().split(' ');
+let a = parseInt(arr[0]);
+let b = parseInt(arr[1]);
+console.log(a+b);
