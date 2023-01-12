92e3b07b865df9fefa4463b41c618b25527ab0a7
wYuYw <83063691+wYuYw@users.noreply.github.com>
2023-01-13 05:29:15 +0900

    Create Baekjoon_1008.java

---
+++ Baekjoon/Basic/Baekjoon_1008.java
@@ -0,0 +1,14 @@
+// Baekjoon_1008 A/B
+
+// Solution by wYuYw
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println(a / b);
+    }
+}
