074456f20ff15f63aa368ffd8aaa0e81dfd954d5
wYuYw <83063691+wYuYw@users.noreply.github.com>
2023-01-26 20:31:25 +0900

    Create Baekjoon_1157.go

---
+++ Baekjoon/Basic/Baekjoon_1157.go
@@ -0,0 +1,49 @@
+// Baekjoon_1157 단어 공부
+
+// Solution by wYuYw
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var input, res string
+	var maxVal int
+
+	reader := bufio.NewReader(os.Stdin)
+	input, _ = reader.ReadString('\n')
+	input = strings.TrimSuffix(input, "\n")
+	input = strings.ToUpper(input)
+
+	counter := make(map[string]int)
+	for _, c := range input {
+		// fmt.Println(reflect.TypeOf(c))
+		// fmt.Println(c) -> return ASCII
+		// fmt.Printf("%v", string(c))
+		if string(c) != " " {
+			counter[string(c)]++
+		}
+	}
+
+	// fmt.Println(input)
+	chars := make([]string, 0, len(counter))
+	for chr := range counter {
+		chars = append(chars, chr)
+	}
+
+	for character, occurrence := range counter {
+		if occurrence > maxVal {
+			maxVal = occurrence
+			res = string(character)
+		} else if occurrence == maxVal {
+			res = "?"
+		}
+	}
+
+	fmt.Println(res)
+}
